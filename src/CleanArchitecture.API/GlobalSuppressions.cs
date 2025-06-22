5ce3468aa366c4f6b5d21aff8d7683e0c339c478
Javiseeker <javieralonsol@churchofjesuschrist.org>
2025-06-22 14:31:17 +0100

    removed global suppressions

--- src/CleanArchitecture.API/GlobalSuppressions.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-// Suppress CA1052 only for the Program class specifically
-[assembly: SuppressMessage("Design", "CA1052:Static holder types should be Static",
-    Justification = "Program class cannot be static because it's used as a generic type parameter in WebApplicationFactory<Program> for integration testing",
-    Target = "~T:CleanArchitecture.API.Program")]
